a90e6be82ec1450134b2f51b1356df1083efc619
root <root@localhost.localdomain>
2018-07-19 07:43:14 -0400

    adding ids login for session variable for the next checkout

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     $username = $_POST["username"];
     $password = $_POST["password"];
     
-    $statement = mysqli_prepare($con, "SELECT name,username,password FROM clients WHERE username = ?");
+    $statement = mysqli_prepare($con, "SELECT id,name,username,password,phone,email FROM clients WHERE username = ?");
     mysqli_stmt_bind_param($statement, "s", $username);
     mysqli_stmt_execute($statement);
     mysqli_stmt_store_result($statement);
