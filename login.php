529a652476b11fc8fbc964c8b6ac7f38554e2b4e
root <root@ip-172-31-25-195.us-west-2.compute.internal>
2018-07-19 11:58:26 +0000

    resolve error in login

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
     mysqli_stmt_bind_param($statement, "s", $username);
     mysqli_stmt_execute($statement);
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $colName,$colUsername, $colPassword);
+    mysqli_stmt_bind_result($statement,$colId, $colName,$colUsername, $colPassword, $colPhone,$colEmail);
     
     $response = array();
     $response["success"] = false;  
@@ -18,8 +18,11 @@
     while(mysqli_stmt_fetch($statement)){
         if (password_verify($password, $colPassword)) {
             $response["success"] = true;  
+	$response["id"] = $colId;
             $response["name"] = $colName;
             $response["username"] = $colUsername;
+	$response["phone"] = $colPhone;
+	$response["email"] = $colEmail;
            
         }
     }
